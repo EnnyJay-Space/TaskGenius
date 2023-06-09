d75923f2ab1a95535277e1feb3a259ef0ad5bdc7
ennyworld <ennyjay4you@gmail.com>
2023-06-09 09:25:52 +0100

    Created App

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut iusto vitae dolorem quaerat recusandae labore. Quisquam vitae ducimus possimus dolores et? Molestiae aut fugit debitis possimus sunt ipsam voluptate alias.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut iusto vitae dolorem quaerat recusandae labore. Quisquam vitae ducimus possimus dolores et? Molestiae aut fugit debitis possimus sunt ipsam voluptate alias.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut iusto vitae dolorem quaerat recusandae labore. Quisquam vitae ducimus possimus dolores et? Molestiae aut fugit debitis possimus sunt ipsam voluptate alias.</p>
   </div>
 </template>
